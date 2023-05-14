c64c79d84bf0e0388ab9eeae89029f47cc0b49cf
zsigri <77639247+zsigri@users.noreply.github.com>
2023-05-14 15:47:21 +0200

    registration_success.php
    I updated the registration_success.php file

---
+++ registration_success.php
@@ -0,0 +1,18 @@
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title> Successful registration </title>
+    <link rel="stylesheet" href="style.css">
+  </head>
+  <body>
+  <?php include_once("header.php"); ?>
+
+  <div class="container">
+    <h1>Signup</h1>
+    <p>Signup was successful. Click here to <a href="user_login.php">login</a></p>
+  </div>
+    
+
+  </body>
+</html>
