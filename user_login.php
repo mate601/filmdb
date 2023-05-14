298cffd3302f4e063b6752d57a80b0b83244a73b
zsigri <77639247+zsigri@users.noreply.github.com>
2023-05-14 15:51:28 +0200

    user_login.php
    i updated the user login.php file

--- user_login.php
+++ user_login.php
@@ -1,13 +1,16 @@
 <?php
 
+include("database.php");
+
 $is_invalid = FALSE;
 if ($_SERVER["REQUEST_METHOD"] === "POST") {
   /** @var \PDO $db */
   $db = require './database.php';
-  $query = "SELECT * FROM users WHERE email = ?";
+  $email = $_POST['email'];
+  $query = "SELECT * FROM users WHERE email = '$email'";
   $stmt = $db->prepare($query);
   $stmt->execute([
-    $_POST['email'],
+    $_POST['email']
   ]);
   $user = $stmt->fetch(PDO::FETCH_ASSOC);
   $hash = password_hash($_POST['password'], PASSWORD_DEFAULT);
@@ -26,19 +29,44 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title> Registration </title>
-    <link rel="stylesheet" href="">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-KK94CHFLLe+nY2dmCWGMq91rCGa5gtU4mk92HdvYe+M/SXH301p5ILy+dN9+nJOZ" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css" type="text/css">
   </head>
+
   <body>
-  <h1>Login</h1>
-  <?php if ($is_invalid): ?>
+
+  <?php include_once("header.php"); ?>
+    
+    
+    <div class="container">
+
+      <div><h1>Login</h1></div>
+      <br><br>
+
+      <?php if ($is_invalid): ?>
+
       <em>Invalid login.</em>
-  <?php endif; ?>
-  <form method="post">
-    <label for="email">Email</label>
-    <input type="email" name="email" id="email" value="<?= htmlspecialchars($_POST['email'] ?? "")?>">
-    <label for="password">Password</label>
-    <input type="password" name="password" id="password">
-    <button type="submit">Log in</button>
-  </form>
+      
+
+      <?php endif; ?>
+
+      <form method="post">
+        <div class="form-group">
+          <label for="email">Email</label>
+          <input type="email" name="email" class="form-control" id="email" value="<?= htmlspecialchars($_POST  ['email'] ?? "")?>">
+        </div>
+        <div class="form-group">
+          <label for="password">Password</label>
+          <input type="password" name="password" id="password" class="form-control">
+      </div>
+      
+      <div class="form-group">
+        <button type="submit" value="login" class="btn btn-primary mt-3 ">Log in</button>
+        <a href="registration.php"><label for="SignUp">Sign Up Here</label></a>
+      </div>
+      </form>
+    </div>
+  
 </body>
 </html>
